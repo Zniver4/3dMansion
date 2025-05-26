40b8500008814226287a5c24252b6c00b5876fdf
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2025-05-25 23:59:30 -0600

    ChestManager
    Random chest can be active in the scene

---
+++ Assets/Scripts/Chest/ChestPulling.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ChestPulling : MonoBehaviour
+{
+    public GameObject[] Chests;
+
+    private int ListLenght = 3;
+    public int MaxPlayableChests = 3;
+
+    void Start()
+    {
+        ListLenght = Chests.Length;
+
+        TurnOffChests();
+
+        ActivateChest();
+    }
+
+    private void StartCoroutine(IEnumerable enumerable)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void TurnOffChests()
+    {
+        for (int listIndex = 0; listIndex < ListLenght; listIndex++)
+        {
+            Chests[listIndex].SetActive(false);
+        }
+    }
+
+    IEnumerable ActivateChest()
+    {
+        int activos = 0;
+
+        while (activos < MaxPlayableChests)
+        {
+            activos = 0;
+
+            foreach (var obj in Chests)
+            {
+                if (obj.activeSelf) activos++;
+            }
+
+            if (activos >= MaxPlayableChests)
+                break;
+
+            GameObject[] inactivos = System.Array.FindAll(Chests, InactiveChest => !InactiveChest.activeSelf);
+
+            int randomIndex = Random.Range(0, inactivos.Length);
+            inactivos[randomIndex].SetActive(true);
+        }
+
+        return null;
+    }
+}
